c532d667dc4c34abfa9aaa0c2a608d4ace3fd361
Renzo Gaspary <rgaspary@gmail.com>
2016-03-14 20:26:57 -0400

    Updating the sticky footer to use vanilla JS instead of jQuery.

--- README.txt
+++ README.txt
@@ -1,18 +1,5 @@
-jQuery Sticky Footer
+JavaScript Sticky footer
 
-I originally found the code at CSS-Tricks.com (http://css-tricks.com/snippets/jquery/jquery-sticky-footer/), but the code is not really working well, so I fixed it and have now posted it here. Feel free to use it as you want, just reference back to me in the code comments.
+This function will set any footer to the bottom of the page using vanilla JavaScript. No jQuery required.
 
-
-
-jQuery code designed to keep the footer at the bottom of the page.
-HTML
-Just make sure the #footer is the last visible thing on your page.
-
-<div id="footer">
-    Sticky Footer
-</div>
-Also make sure that your header and content areas are properly defined as ID's.
-CSS
-Giving it a set height is the most fool-proof.
-
-#footer { height: 100px; width: 100%; }
+Just pass in the footer identifier and call the function and you are done.
--- sticky_footer.js
+++ sticky_footer.js
@@ -1,41 +1,14 @@
-$(window).bind("load", function() {      
-  var footerHeight = 0,
-  footerTop = 0,
-  $footer = $("#footer");
-  $header = $("#header");
-  $content = $("#content");
-  positionFooter();
-  function positionFooter() {
-    footerHeight = $footer.height();
-    footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
-    header1 = $header.height();
-    content1 = $content.height();
+"use strict";
 
-    // DEBUGGING STUFF
-
-    /*console.log("Document height: ", $(document.body).height());
-    console.log("Window height: ", $(window).height());
-    console.log("Window scroll: ", $(window).scrollTop());
-    console.log("Footer height: ", footerHeight);
-    console.log("Footer top: ", footerTop);
-    console.log("-----------")
-    console.log("Header height: ", header1);
-    console.log("Content height: ", content1);
-    console.log("-----------")*/
-
-    if ( (header1+content1+footerHeight) < $(window).height()) {
-      $footer.css({
-        position: "absolute"
-      }).stop().animate({
-        top: footerTop
-      })
-    } else {
-      $footer.css({
-        position: "static"
-      })
+function footerBottom(el) {
+    var windowHeight = window.innerHeight,
+        position = el.offsetTop;
+    if (windowHeight > position) {
+        el.style = "position: absolute; bottom: 0;";
     }
-  }
-  $(window)
-  .scroll(positionFooter)
-  .resize(positionFooter)   
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var footerEl = document.querySelector('#footer');
+    footerBottom(footerEl);
 });
